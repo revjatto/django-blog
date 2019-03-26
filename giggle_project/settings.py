39e073600b50e9d26a1a380a5ee678b7fc588aea
revjatto <revjatto@yahoo.co.uk>
2019-03-26 22:23:17 +0000

    More setting for heroku deployment

--- giggle_project/settings.py
+++ giggle_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 's&%9ux5bogwmbx$@_hg*&_kxf*_4!rp+vgu6h^4rf^y#!c=!**'
-
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
