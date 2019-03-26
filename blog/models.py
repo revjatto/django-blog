38ab17f52a9215b9805a43ecaf82dda76688351a
revjatto <revjatto@yahoo.co.uk>
2019-03-26 21:36:44 +0000

    Password Reset functionality

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -17,3 +18,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model): # new
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=200)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog/comments', null=True)
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('home')
